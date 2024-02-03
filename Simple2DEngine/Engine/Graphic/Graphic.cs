cffa84bb9ae73651d537faae976479a51f6758ee
Vitalii Pyvovarchuk <vitalik.pyvovarchuk@gmail.com>
2024-02-03 15:40:24 +0200

    create TexturesOperating file

--- Simple2DEngine/Engine/Graphic/Graphic.cs
+++ Simple2DEngine/Engine/Graphic/Graphic.cs
@@ -10,56 +10,6 @@ namespace Engine2D
 {
   public partial class Engine : WindowManager
   {
-
-    public static nint GetTexture(string name)
-    {
-      if (_textures.TryGetValue(name, out nint texture))
-      {
-        return texture;
-      } else {
-        return _textures["MissingTexture"];
-      }
-    }
-
-    /// <summary>
-    /// Load textures to window GetRenderer()
-    /// </summary>
-    /// <param name="windowId">Id of window to add</param>
-    /// <param name="directoryPath">Path to directory where texture are hidden, 
-    ///                             for example: "./Engine/standartth/images/". </param>
-    /// <param name="fileNames">Array of texture files' names, with extension, 
-    ///                         for example: "MissingTexture.png".</param>
-    public static void LoadTextures(uint windowId, string directoryPath, string[] fileNames)
-    {
-      foreach (string fileName in fileNames)
-      {
-        LoadTexture(windowId, directoryPath, fileName);
-      }
-    }
-
-    /// <summary>
-    /// Load texture to window GetRenderer()
-    /// </summary>
-    /// <param name="windowId">Id of window to add</param>
-    /// <param name="directoryPath">Path to directory where texture are hidden, 
-    ///                             for example: "./Engine/standartth/images/". </param>
-    /// <param name="fileName">Name of texture file, with extension, 
-    ///                        for example: "MissingTexture.png".</param>
-    public static void LoadTexture(uint windowId, string directoryPath, string fileName)
-    {
-      _textures
-        [ fileName[.. ^(fileName.Length - fileName.LastIndexOf("."))] ] =
-        IMG_LoadTexture(_windows[windowId].GetRenderer(), directoryPath + fileName);
-    }
-
-    private static void DestroyTextures()
-    {
-      foreach (var texture in _textures)
-      {
-        SDL_DestroyTexture(texture.Value);
-      }
-    }
-    
     public static partial class Graphic
     {
       private static uint backgroundColor;
---
+++ Simple2DEngine/Engine/Graphic/TexturesOperating.cs
@@ -0,0 +1,64 @@
+﻿using static SDL2.SDL;
+using static SDL2.SDL_image;
+using static Engine2D.Engine.Graphic.DrawingMethods;
+using System;
+using WM;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Drawing;
+namespace Engine2D
+{
+  public partial class Engine : WindowManager
+  {
+
+    public static nint GetTexture(string name)
+    {
+      if (_textures.TryGetValue(name, out nint texture))
+      {
+        return texture;
+      } else {
+        return _textures["MissingTexture"];
+      }
+    }
+
+    /// <summary>
+    /// Load textures to window GetRenderer()
+    /// </summary>
+    /// <param name="windowId">Id of window to add</param>
+    /// <param name="directoryPath">Path to directory where texture are hidden, 
+    ///                             for example: "./Engine/standartth/images/". </param>
+    /// <param name="fileNames">Array of texture files' names, with extension, 
+    ///                         for example: "MissingTexture.png".</param>
+    public static void LoadTextures(uint windowId, string directoryPath, string[] fileNames)
+    {
+      foreach (string fileName in fileNames)
+      {
+        LoadTexture(windowId, directoryPath, fileName);
+      }
+    }
+
+    /// <summary>
+    /// Load texture to window GetRenderer()
+    /// </summary>
+    /// <param name="windowId">Id of window to add</param>
+    /// <param name="directoryPath">Path to directory where texture are hidden, 
+    ///                             for example: "./Engine/standartth/images/". </param>
+    /// <param name="fileName">Name of texture file, with extension, 
+    ///                        for example: "MissingTexture.png".</param>
+    public static void LoadTexture(uint windowId, string directoryPath, string fileName)
+    {
+      _textures
+        [ fileName[.. ^(fileName.Length - fileName.LastIndexOf("."))] ] =
+        IMG_LoadTexture(_windows[windowId].GetRenderer(), directoryPath + fileName);
+    }
+
+    private static void DestroyTextures()
+    {
+      foreach (var texture in _textures)
+      {
+        SDL_DestroyTexture(texture.Value);
+      }
+    }
+    
+  }
+}
